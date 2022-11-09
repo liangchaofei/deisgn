02a811d934e27da937dd0bbd47fe400bd9e04b85
liangchaofei <chaofei_liang@126.com>
2022-11-09 14:02:46 +0800

    feat: button test

--- src/button/Button.stories.tsx
+++ src/button/Button.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Button } from './Button';
+import Button from '.';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -19,23 +19,38 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  primary: true,
-  label: 'Button',
+  type: 'primary',
+  children: 'Button',
 };
+
+const style = {
+  marginLeft: 8
+}
+
+export const Basic = () => {
+  return <>
+  <Button type="primary">Primary Button</Button>
+  <Button style={style}>Default Button</Button>
+  <Button type="dashed" style={style}>Dashed Button</Button>
+  <br />
+  <Button type="text" style={style}>Text Button</Button>
+  <Button type="link">Link Button</Button>
+</>
+}
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+  children: 'Button',
 };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
+// export const Large = Template.bind({});
+// Large.args = {
+//   size: 'large',
+//   label: 'Button',
+// };
 
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+// export const Small = Template.bind({});
+// Small.args = {
+//   size: 'small',
+//   label: 'Button',
+// };
--- src/button/index.tsx
+++ src/button/index.tsx
@@ -3,21 +3,26 @@ import classnames from 'classnames'
 
 import './index.css';
 
-interface buttonProps{
+interface buttonProps extends React.HTMLAttributes<HTMLButtonElement>{
     className?: string;
-    type?: 'normal' | 'primary';
+    type?: 'normal' | 'primary' | 'dashed' | 'text' | 'link';
+    size?: 'small' | 'medium' | 'large';
     children?: ReactNode;
+    style?: React.CSSProperties;
+    onClick?: React.MouseEventHandler<HTMLButtonElement>;
+    onBlur?: React.FocusEventHandler<HTMLButtonElement>;
 }
 
 const Button = (props: buttonProps) => {
-    const {children, type = 'normal', className} = props;
+    const {children, size = 'medium', type = 'normal', className, style, onClick, onBlur, ...others } = props;
 
     const cls = classnames({
         'ant-btn': true,
         [`ant-btn-${type}`]: type,
+        [`ant-btn-${size}`]: size,
         [className as string]: !!className
     })
-    return <button className={cls}>{children}</button>
+    return <button {...others} onClick={onClick} onBlur={onBlur} className={cls} style={style}>{children}</button>
 }
 
 export default Button;
